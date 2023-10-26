bec72b7295ee0810c6f09f3b973d26c789746a7e
FolashadeOmoloja <omolojashade@gmail.com>
2023-10-26 17:17:08 +0100

    changes

--- components/ImageCarousel.tsx
+++ components/ImageCarousel.tsx
@@ -1,19 +1,17 @@
 "use client"
 import { motion } from 'framer-motion';
+import { useEffect, useState } from 'react';
 
-const images = [{
-    source:'cta-stroke1.svg',
-    height: '10px',
-    delay: 0
-},{
-    source:'cta-stroke2.svg',
+const images = [
+  {
+    source: 'cta-stroke2.svg',
     height: '427px',
-    delay: 1
-},{
-    source:'cta-stroke3.svg',
+  },
+  {
+    source: 'cta-stroke3.svg',
     height: '351px',
-    delay:2
-}];
+  },
+];
 
 const ImageCarousel = () => {
   const imageVariants = {
@@ -21,21 +19,52 @@ const ImageCarousel = () => {
     visible: { opacity: 1 },
   };
 
-  const transition = { duration: 1, delay: 1, repeat: Infinity };
+  const [delay, setDelay] = useState(0);
+  const [display, setDisplay] = useState('hidden')
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setDelay((prevIndex) => (prevIndex + 1) % images.length);
+
+    }, 1000); // Change image every 3 seconds
+
+    return () => {
+      clearInterval(interval);
+    };
+  }, []);
+
+  
+  useEffect(() => {
+    console.log(delay)
+    delay==1?setDisplay('block'):setDisplay('hidden')
+  }, [delay]);
+
 
   return (
-    <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center'}} >
-      {images.map((item, index) => (
+    <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+        <div
+
+        >
+          <img src={'cta-stroke1.svg'} alt={`Image1`} height={"10px"} />
+        </div>
         <motion.div
-          key={index}
           initial="hidden"
           animate="visible"
           variants={imageVariants}
-          transition={{ duration: 1, delay: item.delay, repeat: Infinity }}
+          transition={{ duration: 1, delay: delay, repeat: Infinity, repeatType: 'loop' }}
+
         >
-          <img src={item.source} alt={`Image ${index + 1}`}  height={item.height} />
+          <img src={images[0].source} alt={`Image1`} height={images[0].height} />
         </motion.div>
-      ))}
+        <motion.div
+          initial="hidden"
+          animate="visible"
+          variants={imageVariants}
+          transition={{ duration: 2, delay: delay+1, repeat: Infinity, repeatType: 'loop' }}
+          className={`${display} `}
+        >
+          <img src={images[1].source} alt={`Image1`} height={images[1].height} />
+        </motion.div>
     </div>
   );
 };
