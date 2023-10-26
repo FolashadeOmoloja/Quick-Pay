55c293eb00acbe22666ef16f2383a79c529f2320
FolashadeOmoloja <omolojashade@gmail.com>
2023-10-26 13:59:42 +0100

    changes

--- components/CTA.tsx
+++ components/CTA.tsx
@@ -2,8 +2,10 @@ import React from 'react'
 
 const CTA = () => {
   return (
-    <section>
-        
+    <section className='h-[500px] w-full bg-[#0083C7]'>
+         <div className='w-[272px]'>
+
+         </div>
     </section>
   )
 }
