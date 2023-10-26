9ceddbdbf8e75ec1beb1873fa65a4d17b4c3c04c
FolashadeOmoloja <omolojashade@gmail.com>
2023-10-26 21:13:01 +0100

    changes

--- components/ImageCarousel.tsx
+++ components/ImageCarousel.tsx
@@ -14,57 +14,19 @@ const images = [
 ];
 
 const ImageCarousel = () => {
-  const imageVariants = {
-    hidden: { opacity: 0 },
-    visible: { opacity: 1 },
-  };
-
-  const [delay, setDelay] = useState(0);
-  const [display, setDisplay] = useState('hidden')
-
-  useEffect(() => {
-    const interval = setInterval(() => {
-      setDelay((prevIndex) => (prevIndex + 1) % images.length);
-
-    }, 1000); // Change image every 3 seconds
-
-    return () => {
-      clearInterval(interval);
-    };
-  }, []);
-
-  
-  useEffect(() => {
-    console.log(delay)
-    delay==1?setDisplay('block'):setDisplay('hidden')
-  }, [delay]);
 
 
   return (
-    <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
-        <div
-
-        >
-          <img src={'cta-stroke1.svg'} alt={`Image1`} height={"10px"} />
-        </div>
-        <motion.div
-          initial="hidden"
-          animate="visible"
-          variants={imageVariants}
-          transition={{ duration: 1, delay: delay, repeat: Infinity, repeatType: 'loop' }}
-
-        >
-          <img src={images[0].source} alt={`Image1`} height={images[0].height} />
-        </motion.div>
-        <motion.div
-          initial="hidden"
-          animate="visible"
-          variants={imageVariants}
-          transition={{ duration: 2, delay: delay+1, repeat: Infinity, repeatType: 'loop' }}
-          className={`${display} `}
-        >
-          <img src={images[1].source} alt={`Image1`} height={images[1].height} />
-        </motion.div>
+    <div className='h-full justify-end items-center flex relative'>
+      <div className='absolute right-[272px] bottom-[20%]'>
+        <img src={'cta-stroke1.svg'} alt={`Image1`} height={'10px'} />
+      </div>
+      <div className='absolute right-[232px] bottom-0 ellipsis'>
+        <img src={images[0].source} alt={`Image1`} height={images[0].height} />
+      </div>
+      <div className='absolute top-0 ellipsis2'>
+        <img src={images[1].source} alt={`Image2`} height={images[1].height} />
+      </div>
     </div>
   );
 };
