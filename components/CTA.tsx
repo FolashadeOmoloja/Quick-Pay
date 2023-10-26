b65b1fe0cf08c8bbeb87a75a162df9e601a9808f
FolashadeOmoloja <omolojashade@gmail.com>
2023-10-26 21:58:07 +0100

    changes

--- components/CTA.tsx
+++ components/CTA.tsx
@@ -3,11 +3,21 @@
 //272px
 
 import ImageCarousel from "./ImageCarousel"
+import TopStrokes from "./TopStrokes"
 
 const CTA = () => {
   return (
-    <section className='h-[500px] w-full bg-[#0083C7] mt-[60px]'>
-      <ImageCarousel/>
+    <section className='h-[500px] w-full bg-[#0083C7] mt-[60px] relative'>
+        <section>
+             <h3 className="text-white">Download QuickPay App Now!!</h3>
+        </section>
+         <div className="w-full h-full absolute top-0">
+         <TopStrokes/>
+         </div>
+         <div className="w-full h-full absolute top-0">
+         <ImageCarousel/>
+         </div>
+
     </section>
   )
 }
---
+++ components/TopStrokes.tsx
@@ -0,0 +1,11 @@
+
+
+const TopStrokes = () => {
+  return (
+    <div>
+        <img src="cta-stroke3.svg" alt="" className="ellipsis"/>
+    </div>
+  )
+}
+
+export default TopStrokes
