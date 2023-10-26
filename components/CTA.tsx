0362ce7693d41b3e39121e245d9c854b9bfd40e9
FolashadeOmoloja <omolojashade@gmail.com>
2023-10-27 00:06:29 +0100

    changes

--- components/CTA.tsx
+++ components/CTA.tsx
@@ -2,6 +2,7 @@
 
 //272px
 
+import CTAButton from "./CTAButton"
 import ImageCarousel from "./ImageCarousel"
 import TopStrokes from "./TopStrokes"
 
@@ -10,6 +11,7 @@ const CTA = () => {
     <section className='h-[500px] w-full bg-[#0083C7] mt-[60px] relative p-[70px]'>
         <section className="">
              <h3 className="text-white text-[50px] font-bold">Download QuickPay App <br /> Now!!</h3>
+             <CTAButton/>
         </section>
          <div className="w-full h-full absolute top-0">
          <TopStrokes/>
---
+++ components/CTAButton.tsx
@@ -0,0 +1,36 @@
+import React from 'react'
+
+const button =[
+    {
+        img: 'apple.svg',
+        text: 'Apple Store'
+    },
+    {
+        img: 'playstore.svg',
+        text: 'Google Play'
+    }
+]
+
+const CTAButton = () => {
+  return (
+   <section className='flex gap-6 text-[#0045A5]'>
+         {
+            button.map((item,index)=>{
+                return     (
+                <div key={index} className='flex w-[177px] h-[55px] bg-white rounded-[40px] items-center justify-center'>
+                     <div>
+                        <img src={item.img} alt="img" />
+                     </div>
+                     <div className='flex flex-col'>
+                      <span className='text-[12px]'>Get QuickPay on</span>
+                      <span className='font-bold'>{item.text}</span>
+                         
+                     </div>
+            </div>)
+            })
+         }
+   </section>
+  )
+}
+
+export default CTAButton
