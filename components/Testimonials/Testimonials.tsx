2edcf2939cda35bab4146f784c86cac58a074b73
FolashadeOmoloja <omolojashade@gmail.com>
2023-10-28 20:44:37 +0100

    testimonials

--- components/Testimonials/Testimonials.tsx
+++ components/Testimonials/Testimonials.tsx
@@ -1,23 +1,26 @@
+import {BsStarFill } from 'react-icons/bs'
 const users = [
 {
-    img: 'user1',
+    img: 'user1.svg',
     user: 'John Doe',
     text: '"QuickPay has truly revolutionized how I manage my finances. With the ability to pay utility bills and recharge my phone credit in just a few clicks, my daily life has become remarkably convenient.”'
 
+},
+{
+    img: 'user2.svg',
+    user: 'Jane Smith',
+    text: `"QuickPay is the ultimate solution for anyone who values efficiency. The platform's secure and straightforward approach to bill payments and airtime recharges has transformed the way I handle my everyday expenses.”`
+
+},
+{
+    img: 'user3.svg',
+    user: 'Alogbe Rachael',
+    text: `"I have full trust in QuickPay for all my payment needs. It's more than just a convenient tool; it's a reliable companion in my financial journey. The peace of mind knowing my transactions are secure is the icing on the cake.”`
 }
+
 ]
 
-// const stars = [];
 
-// for (let i = 0; i < 5; i++) {
-//   if (i < integerPart) {
-//     stars.push(<BsStarFill key={i} color="#29AFFD" fontSize='14px' />);
-//   } else if (i === integerPart && decimalPart >= 0.3 && decimalPart <= 0.7) {
-//     stars.push(<BsStarHalf key={i} color="#29AFFD" fontSize='14px' />);
-//   } else {
-//     stars.push(<BsStar key={i} color="#29AFFD" fontSize='14px' />);
-//   }
-// }
 
 const Testimonials = () => {
   return (
@@ -31,7 +34,26 @@ const Testimonials = () => {
                  <img src="rightbtn.svg" alt="" />
              </div>
              <section className="flex gap-5">
-     
+                 {
+                    users.map((user, index)=>{
+                        const stars = [];
+
+                        for (let i = 0; i < 5; i++) {
+                            stars.push(<BsStarFill key={i} color="#FFA500" fontSize='24px' />);
+                          }
+
+                        return(
+                             <div className="relative flex flex-col" key={index}>
+                                  <div>
+                                     <img src={user.img} alt="img" />
+                                  </div>
+                                  <span>{user.user}</span>
+                                  <p>{user.text}</p>
+                                  <div className='flex gap-4'>{stars}</div>
+                             </div>
+                        )
+                    })
+                 }
                   
            </section>
         </section>
