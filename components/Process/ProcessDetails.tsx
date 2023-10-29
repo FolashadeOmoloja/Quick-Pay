4743c838be85c5e37c7088a20427e17373944e04
FolashadeOmoloja <omolojashade@gmail.com>
2023-10-29 22:57:19 +0100

    process

--- components/Process/ProcessDetails.tsx
+++ components/Process/ProcessDetails.tsx
@@ -3,17 +3,64 @@ import {motion} from 'framer-motion'
 
 const ProcessDetails = () => {
   return (
-        <section>
-            <section className='flex'>
-               <div className='bg-white py-[48px] px-8 flex gap-[52px] rounded-[20px] shadow-[-10px_-10px_30px_4px_rgba(0,0,0,0.1),_10px_10px_30px_4px_rgba(0,69,165,0.2)]'>
-                       <div className='w-[60px] h-[60px] border-4 border-[#0045A5] rounded-full flex items-center justify-center text-[#0045A5] font-bold text-[20px]'>1</div>
+        <section className='flex flex-col gap-[35px]'>
+            <section className='flex relative'>
+               <div className='bg-white py-[48px] px-6 flex gap-[40px] rounded-[20px] shadow-[-10px_-10px_30px_4px_rgba(0,0,0,0.1),_10px_10px_30px_4px_rgba(0,69,165,0.2)] w-[50%]'>
+                       <div className=' '>
+                        <img src="circle1.svg" alt="img" />
+                       </div>
                        <div>
                            <p className='font-bold mb-[14px] text-[#333333]'>Choose Your Service Description</p>
                            <p className='text-sm text-[#999999]'>Pick from a wide array of services, whether it's utility bills, airtime top-ups, or more.</p>
                        </div>
                   </div>
-                  <div className='w-[300px] h-[2px] bg-[#0045A5] '></div>
+                  <div className='absolute top-[100px] left-[50%] '>
+                    <img src="processline1.svg" alt="" />
+                  </div>
             </section>
+            {/*second div*/}
+            <section className='flex justify-end relative'>
+                     <div className='absolute top-[100px] right-[50%] '>
+                       <img src="processline2.svg" alt="" />
+                      </div>
+                       <div className='bg-white py-[48px] px-8 flex gap-[52px] rounded-[20px] shadow-[-10px_-10px_30px_4px_rgba(0,0,0,0.1),_10px_10px_30px_4px_rgba(0,69,165,0.2)] w-[50%]'>
+                       <div className=''>
+                        <img src="circle2.svg" alt="img" />
+                       </div>
+                       <div>
+                           <p className='font-bold mb-[14px] text-[#333333]'>Enter Details Description</p>
+                           <p className='text-sm text-[#999999]'>Fill in essential information like your account details, phone number, and the amount to recharge or pay</p>
+                       </div>
+                  </div>
+            </section>
+            {/*third div*/}
+            <section className='flex relative'>
+               <div className='bg-white py-[48px] px-6 flex gap-[40px] rounded-[20px] shadow-[-10px_-10px_30px_4px_rgba(0,0,0,0.1),_10px_10px_30px_4px_rgba(0,69,165,0.2)] w-[50%]'>
+                       <div className=' '>
+                        <img src="circle3.svg" alt="img" />
+                       </div>
+                       <div>
+                           <p className='font-bold mb-[14px] text-[#333333]'>Review & Confirm Description</p>
+                           <p className='text-sm text-[#999999]'>Carefully double-check your inputs, and with a simple tap, confirm your transaction.</p>
+                       </div>
+                  </div>
+                  <div className='absolute top-[100px] left-[50%] '>
+                    <img src="processline1.svg" alt="" />
+                  </div>
+            </section>
+            {/*fourth div*/}
+            <section className='flex justify-end relative'>
+                       <div className='bg-white py-[48px] px-8 flex gap-[52px] rounded-[20px] shadow-[-10px_-10px_30px_4px_rgba(0,0,0,0.1),_10px_10px_30px_4px_rgba(0,69,165,0.2)] w-[50%]'>
+                       <div className=''>
+                        <img src="circle4.svg" alt="img" />
+                       </div>
+                       <div>
+                           <p className='font-bold mb-[14px] text-[#333333]'>Instant Confirmation Description</p>
+                           <p className='text-sm text-[#999999]'>Get immediate confirmation of your payment or recharge - quick, easy, and hassle-free!</p>
+                       </div>
+                  </div>
+            </section>
+
         </section>
         
   )
