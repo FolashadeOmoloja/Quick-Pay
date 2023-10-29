7e075d0816aa00bfe524fff0690d241aec9f52f1
FolashadeOmoloja <omolojashade@gmail.com>
2023-10-29 20:45:47 +0100

    process

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,12 @@
 import CTA from "@/components/CTA/CTA"
+import Process from "@/components/Process/Process"
 import Testimonials from "@/components/Testimonials/Testimonials"
 
 
 const page = () => {
   return (
     <>
+    <Process/>
     <Testimonials/>
     <CTA/>
     </>
