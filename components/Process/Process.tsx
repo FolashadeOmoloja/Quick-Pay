ad5f21aecbfee7576739f0255de7883f30f01d63
FolashadeOmoloja <omolojashade@gmail.com>
2023-10-29 20:41:22 +0100

    process

---
+++ components/Process/Process.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Process = () => {
+  return (
+         <section className='pt-[154px] bg-white pb-[100px] px-[72px] max-sm:p-[72px] max-xmd:p-[20px]'>
+             <h3 className="text-[50px] max-md:text-[30px] font-bold max-sm:text-[40px] max-sm:text-center">Our Testimonial</h3>
+             <div className="w-[115px] h-[3px] bg-[#0045A5] mb-6 max-sm:mx-auto"></div>
+             <p className="text-[#565252] font-semibold mb-[52px] max-sm:text-center">What Our Users Say</p>
+         </section>
+  )
+}
+
+export default Process
