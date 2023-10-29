71919324fcfdf0ff835968691bc70826c7de90ef
FolashadeOmoloja <omolojashade@gmail.com>
2023-10-29 23:22:16 +0100

    process

--- components/Process/Process.tsx
+++ components/Process/Process.tsx
@@ -3,8 +3,8 @@ import ProcessDetails from './ProcessDetails'
 
 const Process = () => {
   return (
-         <section className='py-[100px] bg-gradient px-[72px] max-sm:p-[72px] max-xmd:p-[20px]'>
-               <section className='text-center mb-[25px] '>
+         <section className='py-[100px] bg-gradient px-[72px] max-sm:p-[72px] max-xmd:p-[20px] max-xmd:pb-[70px]'>
+               <section className='text-center mb-[40px] '>
                   <h3 className="text-[50px] text-[#333333] max-md:text-[30px] font-bold max-sm:text-[40px]">How It Works</h3>
                   <div className="w-[115px] h-[3px] bg-[#0045A5] mb-6 mx-auto"></div>
                    <p className="text-[#565252] font-semibold ">Simplicity in Action</p>
--- components/Process/ProcessDetails.tsx
+++ components/Process/ProcessDetails.tsx
@@ -3,9 +3,9 @@ import {motion} from 'framer-motion'
 
 const ProcessDetails = () => {
   return (
-        <section className='flex flex-col gap-[35px]'>
+        <section className='flex flex-col gap-[35px] mt-6'>
             <section className='flex relative'>
-               <div className='bg-white py-[48px] px-6 flex gap-[40px] rounded-[20px] shadow-[-10px_-10px_30px_4px_rgba(0,0,0,0.1),_10px_10px_30px_4px_rgba(0,69,165,0.2)] w-[50%]'>
+               <div className='bg-white py-[48px] px-8 max-xmd:px-4 flex gap-[40px] max-xmd:gap-[20px] rounded-[20px] shadow-[-10px_-10px_30px_4px_rgba(0,0,0,0.1),_10px_10px_30px_4px_rgba(0,69,165,0.2)] w-[50%] max-md:w-full  '>
                        <div className=' '>
                         <img src="circle1.svg" alt="img" />
                        </div>
@@ -14,16 +14,16 @@ const ProcessDetails = () => {
                            <p className='text-sm text-[#999999]'>Pick from a wide array of services, whether it's utility bills, airtime top-ups, or more.</p>
                        </div>
                   </div>
-                  <div className='absolute top-[100px] left-[50%] '>
+                  <div className='absolute top-[100px] left-[50%] max-lg:hidden  '>
                     <img src="processline1.svg" alt="" />
                   </div>
             </section>
             {/*second div*/}
             <section className='flex justify-end relative'>
-                     <div className='absolute top-[100px] right-[50%] '>
+                     <div className='absolute top-[100px] right-[50%] max-lg:hidden '>
                        <img src="processline2.svg" alt="" />
                       </div>
-                       <div className='bg-white py-[48px] px-8 flex gap-[52px] rounded-[20px] shadow-[-10px_-10px_30px_4px_rgba(0,0,0,0.1),_10px_10px_30px_4px_rgba(0,69,165,0.2)] w-[50%]'>
+                      <div className='bg-white py-[48px] px-8 max-xmd:px-4 flex gap-[40px] max-xmd:gap-[20px] rounded-[20px] shadow-[-10px_-10px_30px_4px_rgba(0,0,0,0.1),_10px_10px_30px_4px_rgba(0,69,165,0.2)] w-[50%] max-md:w-full  '>
                        <div className=''>
                         <img src="circle2.svg" alt="img" />
                        </div>
@@ -35,22 +35,22 @@ const ProcessDetails = () => {
             </section>
             {/*third div*/}
             <section className='flex relative'>
-               <div className='bg-white py-[48px] px-6 flex gap-[40px] rounded-[20px] shadow-[-10px_-10px_30px_4px_rgba(0,0,0,0.1),_10px_10px_30px_4px_rgba(0,69,165,0.2)] w-[50%]'>
-                       <div className=' '>
-                        <img src="circle3.svg" alt="img" />
+               <div className='bg-white py-[48px] px-8 max-xmd:px-4 flex gap-[40px] max-xmd:gap-[20px] rounded-[20px] shadow-[-10px_-10px_30px_4px_rgba(0,0,0,0.1),_10px_10px_30px_4px_rgba(0,69,165,0.2)] w-[50%] max-md:w-full  '>
+                       <div >
+                        <img src="circle3.svg" alt="img" className=''/>
                        </div>
                        <div>
                            <p className='font-bold mb-[14px] text-[#333333]'>Review & Confirm Description</p>
                            <p className='text-sm text-[#999999]'>Carefully double-check your inputs, and with a simple tap, confirm your transaction.</p>
                        </div>
                   </div>
-                  <div className='absolute top-[100px] left-[50%] '>
+                  <div className='absolute top-[100px] left-[50%] max-lg:hidden   '>
                     <img src="processline1.svg" alt="" />
                   </div>
             </section>
             {/*fourth div*/}
             <section className='flex justify-end relative'>
-                       <div className='bg-white py-[48px] px-8 flex gap-[52px] rounded-[20px] shadow-[-10px_-10px_30px_4px_rgba(0,0,0,0.1),_10px_10px_30px_4px_rgba(0,69,165,0.2)] w-[50%]'>
+                       <div className='bg-white py-[48px] px-8 max-xmd:px-4 flex gap-[40px] max-xmd:gap-[20px] rounded-[20px] shadow-[-10px_-10px_30px_4px_rgba(0,0,0,0.1),_10px_10px_30px_4px_rgba(0,69,165,0.2)] w-[50%] max-md:w-full  '>
                        <div className=''>
                         <img src="circle4.svg" alt="img" />
                        </div>
