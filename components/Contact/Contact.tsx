96c4fe71412f525088655ee1fc5f470dbfeeacd6
FolashadeOmoloja <omolojashade@gmail.com>
2023-10-30 12:33:44 +0100

    contact

--- components/Contact/Contact.tsx
+++ components/Contact/Contact.tsx
@@ -1,9 +1,15 @@
-import React from 'react'
+
 
 const contact = () => {
   return (
-    <section className='py-[100px] bg-gradient px-[72px] max-sm:p-[72px] max-xmd:p-[20px] max-xmd:pb-[70px] '>
-    <span>conhjdkdk</span>
+    <section className='pt-[200px] pb-[100px]  px-[72px] max-sm:p-[72px] max-xmd:p-[20px] max-xmd:pb-[70px] '>
+      <section className="bg-[url('/contactbg.svg')] bg-cover flex">
+               <section>
+                   <h3 className="text-[50px] text-[#333333] max-md:text-[30px] font-bold max-sm:text-[40px] max-sm:text-center">Contact Us</h3>
+                   <div className="w-[115px] h-[3px] bg-[#0045A5] mb-6 max-sm:mx-auto"></div>
+                   <p className="text-[#565252] font-semibold mb-[52px] max-sm:text-center">Got Questions? Let's Talk</p>
+               </section>
+      </section>
 </section>
   )
 }
