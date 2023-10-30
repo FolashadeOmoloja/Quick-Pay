181dd9943ecaf3c7811e2224f9dbc5b68db861bb
FolashadeOmoloja <omolojashade@gmail.com>
2023-10-30 13:31:22 +0100

    contact

--- components/Contact/Contact.tsx
+++ components/Contact/Contact.tsx
@@ -3,7 +3,7 @@ import ContactForm from "./ContactForm"
 
 const contact = () => {
   return (
-    <section className='pt-[200px] pb-[100px]  max-sm:p-[72px] max-xmd:p-[20px] max-xmd:pb-[70px] '>
+    <section className='pt-[250px]   max-sm:p-[72px] max-xmd:p-[20px] max-xmd:pb-[70px] '>
       <section className="bg-[url('/contactbg.svg')] bg-cover flex gap-[145px] pb-[250px] px-[72px] ">
                <section>
                    <h3 className="text-[50px] text-[#333333] max-md:text-[30px] font-bold max-sm:text-[40px] max-sm:text-center">Contact Us</h3>
--- components/Contact/ContactForm.tsx
+++ components/Contact/ContactForm.tsx
@@ -1,8 +1,39 @@
-
-
 const ContactForm = () => {
   return (
-    <div>ContactForm</div>
+    <section className="py-[62px] px-[42px] bg-white rounded-[20px] shadow-[-10px_-10px_30px_4px_rgba(0,0,0,0.1),_10px_10px_30px_4px_rgba(0,69,165,0.2)]">
+          <form>
+             <h3 className="text-[#333333] font-bold text-[20px] mb-6">Send Us A  Message</h3>
+            <div className="flex gap-8">
+                <div className="flex flex-col gap-3 text-sm" >
+                      <label className="text-[#3A3A3A] font-semibold">Name</label>
+                      <input type="text" 
+                      placeholder="Alogbe Rachael" 
+                      className="text-[#999999] w-[250px] h-[40px] py-3 px-[18px] border border-[#796E6E] rounded-[4px] outline-none focus:border-[#0045A5] focus:border-2"
+                      
+                      />
+                 </div>
+                 <div className="flex flex-col gap-3 text-sm" >
+                      <label className="text-[#3A3A3A] font-semibold">Email</label>
+                      <input type="email" 
+                      placeholder="john@gmail.com" 
+                      className="text-[#999999] w-[250px] h-[40px] py-3 px-[18px] border border-[#796E6E] rounded-[4px] outline-none focus:border-[#0045A5] focus:border-2"
+                      
+                      />
+                 </div>
+            </div>
+            <div className="flex flex-col gap-3 text-sm mt-8" >
+                  <label className="text-[#3A3A3A] font-semibold">Message</label>
+                  <textarea  
+                  placeholder="I need  something..." 
+                  className="text-[#999999] w-full h-[90px] py-3 px-[18px] border border-[#796E6E] rounded-[4px] outline-none focus:border-[#0045A5] focus:border-2 resize-none"
+                  
+                  />
+             </div>
+             <button className="bg-[#0045A5] text-white mt-9 rounded-[40px] h-[51px] w-[157px]">
+                 Send
+             </button>
+          </form>
+    </section>
   )
 }
 
