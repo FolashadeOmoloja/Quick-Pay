687771cd0df64cfb756c164eb0e355c5e10f3a66
FolashadeOmoloja <omolojashade@gmail.com>
2023-10-31 13:12:32 +0100

    navbar

--- components/Navbar/MobileNav.tsx
+++ components/Navbar/MobileNav.tsx
@@ -1,3 +1,4 @@
+"use client"
 import React, { useState } from 'react'
 import { FaPlus } from 'react-icons/fa';
 
@@ -5,40 +6,42 @@ const MobileNav = () => {
     const [activeNav, setActiveNav] = useState ('#')
     const [navClassName, setNavClassName] = useState("nav");
     const [showMenu, setShowMenu] = useState("");
-    const [navBarDisplay, setNavBarDisplay] = useState("none");
+    const [navBarDisplay, setNavBarDisplay] = useState("hidden");
   
     const toggleMenu = () => {
       if (navClassName === "nav") {
-        setNavClassName("nav new_height");
+        setNavClassName("nav new_height fixed");
         setShowMenu("close_menu");
         setNavBarDisplay('flex');
       } else {
         setNavClassName("nav");
         setShowMenu("");
-        setNavBarDisplay('none');
+        setNavBarDisplay('hidden');
       }
     };
   return (
-    <section className='nav bg-gradient z-[150] flex justify-between'>
-         <div className=''>
-             <img src="footerlogo.svg" alt="" />
-         </div>
-         <div className='flex flex-col justify-center items-center'>
-               <ul className="nav-bar flex flex-col gap-8 text-[#1D1C1C] font-semibold text-lg justify-center items-center">
+    <section className={`xlg:hidden px-[72px] pt-[30px] max-xmd:p-[20px] max-xmd:pt-[40px] ${navClassName} bg-gradient z-[150] `}>
+            <section className='flex justify-between items-center'>
+              <div className='max-xmd:w-[150px]'>
+                <img src="footerlogo.svg" alt="" />
+              </div>
+             <div onClick={toggleMenu} className=" text-[#0045A5] text-[32px] max-xsm:text-[24px] xlg:hidden cursor-pointer ">
+                 <FaPlus className={`transition-all duration-400 ease-in ${showMenu}`} />
+              </div>
+            </section>
+            <div className={`${navBarDisplay} flex-col justify-center items-center`}>
+               <ul className="nav-bar flex flex-col gap-6 text-[#1D1C1C] font-semibold text-lg justify-center items-center">
                   <li><a href="#" onClick={()=> setActiveNav('#')} className= {activeNav === '#'? 'nav-active': 'nav-links'}>Home</a></li>
                   <li><a href="#features" onClick={()=> setActiveNav('#features')} className= {activeNav === '#features'? 'nav-active': 'nav-links'}>Features</a></li>
                   <li><a href="#about-us" onClick={()=> setActiveNav('#about-us')} className= {activeNav === '#about-us'? 'nav-active': 'nav-links'}>About Us</a></li>
                   <li><a href="#how-it-works" onClick={()=> setActiveNav('#how-it-works')} className= {activeNav === '#how-it-works'? 'nav-active': 'nav-links'}>How it works</a></li>
                   <li><a href="#contact-us" onClick={()=> setActiveNav('#contact-us')} className= {activeNav === '#contact-us'? 'nav-active': 'nav-links'}>Contact Us</a></li>        
              </ul>
-             <div className='flex gap-6 mt-8'>
+             <div className='flex gap-6 mt-6'>
                 <button className='w-[131px] h-[43px] font-bold  rounded-[40px] text-[#0045A5] border-2 border-[#0045A5] '>Login</button>
                 <button className='w-[131px] h-[43px] font-bold rounded-[40px] text-white bg-[#0045A5] '>Register</button>
             </div>
          </div>
-         <div onClick={toggleMenu} className=" text-[#0045A5] text-[32px] xlg:hidden cursor-pointer">
-            <FaPlus className={`transition-all duration-400 ease-in ${showMenu}`} />
-        </div>
 
     </section>
   )
--- components/Navbar/Navbar.tsx
+++ components/Navbar/Navbar.tsx
@@ -1,31 +1,16 @@
 'use client'
 import {useState} from 'react'
-import {FaPlus} from 'react-icons/fa'
-import MobileNav from './MobileNav'
+
 
 const Navbar = () => {
     const [activeNav, setActiveNav] = useState ('#')
-    const [navClassName, setNavClassName] = useState("nav");
-    const [showMenu, setShowMenu] = useState("");
-    const [navBarDisplay, setNavBarDisplay] = useState("none");
   
-    const toggleMenu = () => {
-      if (navClassName === "nav") {
-        setNavClassName("nav new_height");
-        setShowMenu("close_menu");
-        setNavBarDisplay('flex');
-      } else {
-        setNavClassName("nav");
-        setShowMenu("");
-        setNavBarDisplay('none');
-      }
-    };
   return (
     <nav className='bg-white h-[144px] flex justify-between items-center px-[72px] max-xmd:p-[20px] max-xlg:hidden'>
          <div className=''>
              <img src="footerlogo.svg" alt="" />
          </div>
-         <ul className="nav-bar flex gap-8 text-[#1D1C1C] font-semibold text-lg max-xlg:hidden">
+         <ul className="nav-bar flex gap-8 text-[#1D1C1C] font-semibold text-lg">
             <li><a href="#" onClick={()=> setActiveNav('#')} className= {activeNav === '#'? 'nav-active': 'nav-links'}>Home</a></li>
             <li><a href="#features" onClick={()=> setActiveNav('#features')} className= {activeNav === '#features'? 'nav-active': 'nav-links'}>Features</a></li>
             <li><a href="#about-us" onClick={()=> setActiveNav('#about-us')} className= {activeNav === '#about-us'? 'nav-active': 'nav-links'}>About Us</a></li>
