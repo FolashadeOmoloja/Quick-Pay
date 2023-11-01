0fb7349022582b464a5bb105d0843c5c304c077f
FolashadeOmoloja <omolojashade@gmail.com>
2023-11-01 23:30:34 +0100

    header

--- components/CTA/CTAButton.tsx
+++ components/CTA/CTAButton.tsx
@@ -1,36 +1,53 @@
 import React from 'react'
 
-const button =[
-    {
+
+
+interface IButton {
+    img: string;
+    text: string;
+    title: string;
+  }
+  
+  interface Iprops {
+    button?: IButton[];
+  }
+  
+  const CTAButton: React.FC<Iprops> = ({
+    button = [
+      {
         img: 'apple.svg',
-        text: 'Apple Store'
-    },
-    {
+        text: 'Apple Store',
+        title: 'Get QuickPay on'
+      },
+      {
         img: 'playstore.svg',
-        text: 'Google Play'
-    }
-]
+        text: 'Google Play',
+        title: 'Get QuickPay on'
+      },
+    ],
 
-const CTAButton = () => {
-  return (
-   <section className='flex gap-6 text-[#0045A5] max-sm:flex-col'>
-         {
-            button.map((item,index)=>{
-                return     (
-                <div key={index} className='flex w-[177px] h-[55px] bg-white rounded-[40px] items-center justify-center cursor-pointer'>
-                     <div>
-                        <img src={item.img} alt="img" />
-                     </div>
-                     <div className='flex flex-col'>
-                      <span className='text-[12px]'>Get QuickPay on</span>
-                      <span className='font-bold'>{item.text}</span>
-                         
-                     </div>
-            </div>)
-            })
-         }
-   </section>
-  )
-}
-
-export default CTAButton
+  }) => {
+    return (
+      <section className='flex gap-6 text-[#0045A5] max-sm:flex-col'>
+        {button.map((item, index) => {
+          return (
+            <div
+              key={index}
+              className='flex w-[177px] h-[55px] bg-white rounded-[40px] items-center justify-center cursor-pointer'
+            >
+              <div>
+                <img src={item.img} alt="" />
+              </div>
+              <div className='flex flex-col'>
+                <span className='text-[12px]'>{item.title}</span>
+                <span className='font-bold'>{item.text}</span>
+              </div>
+            </div>
+          );
+        })}
+      </section>
+    );
+  };
+  
+  export default CTAButton;
+  
--- components/Header/FirstSlide.tsx
+++ components/Header/FirstSlide.tsx
@@ -6,20 +6,33 @@ import TopStrokes from "../CTA/TopStrokes"
 
 const FirstSlide = () => {
   return (
-    <section className='h-[800px]  bg-[#0083C7]  relative w-full'>
-        <section className="z-[100] relative flex p-[72px] lg:pt-[297px] pr-0 max-xmd:p-[20px] max-xsm:items-center max-xsm:justify-center h-full">
-              <section className="max-xmd:flex max-xmd:flex-col justify-center items-center">
-              <h3 className="text-white text-[50px] font-bold mb-[60px] max-sm:text-[40px] max-xmd:text-center">Download QuickPay App <br className="max-md:hidden"/> Now!!</h3>
-               <CTAButton/>
+    <section className='h-[830px]  bg-[#0083C7]  relative w-full'>
+        <section className="z-[100] relative flex  p-[72px]  pr-0 max-xmd:p-[20px] items-center max-xsm:justify-center h-full">
+              <section className="flex flex-col text-white max-xmd:justify-center max-xmd:items-center">
+                <span className="text-[32px] mb-1">Simplifying Your Life</span>
+              <h3 className=" text-[72px] font-bold mb-6 underline decoration-1 max-sm:text-[40px] max-xmd:text-center">Pay Bills with Ease!</h3>
+              <p className="mb-[60px]">Say goodbye to the hassle of paying bills. Our platform makes it quick and painless</p>
+               <CTAButton button={[
+                      {
+                        img: '',
+                        text: 'Get Started',
+                        title:''
+                      },
+                      {
+                        img: 'playstore.svg',
+                        text: 'Google Play',
+                        title: 'Get QuickPay on'
+                      },
+              ]} />
               </section>
               <div className="absolute top-[55%] left-[33%] max-slg:top-[55%]  max-md:hidden ">
               <Arrrows/>
               </div>
-              {/* <section className="iphone-cta">
-                <img src="iPhone.svg" alt="" className="w-full" />
-              </section> */}
+              <section className="absolute right-0">
+                <img src="header-first.svg" alt="" className="w-full" />
+              </section>
         </section>
-         <div className="w-full h-[500px] absolute top-0">
+         <div className="w-full h-full absolute top-0 ">
          <ImageCarousel/>
          </div>
 
