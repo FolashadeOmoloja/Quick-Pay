320e73a37d56d486c06cac0674efe672c182df27
FolashadeOmoloja <omolojashade@gmail.com>
2023-11-01 22:25:14 +0100

    header

--- components/Header/FirstSlide.tsx
+++ components/Header/FirstSlide.tsx
@@ -1,8 +1,29 @@
-import React from 'react'
+import Arrrows from "../CTA/Arrrows"
+import CTAButton from "../CTA/CTAButton"
+import ImageCarousel from "../CTA/ImageCarousel"
+import TopStrokes from "../CTA/TopStrokes"
+
 
 const FirstSlide = () => {
   return (
-    <div className='bg-red-500 h-full w-full flex items-center justify-center text-[100px]'>FirstSlide</div>
+    <section className='h-[800px]  bg-[#0083C7]  relative w-full'>
+        <section className="z-[100] relative flex p-[72px] lg:pt-[297px] pr-0 max-xmd:p-[20px] max-xsm:items-center max-xsm:justify-center h-full">
+              <section className="max-xmd:flex max-xmd:flex-col justify-center items-center">
+              <h3 className="text-white text-[50px] font-bold mb-[60px] max-sm:text-[40px] max-xmd:text-center">Download QuickPay App <br className="max-md:hidden"/> Now!!</h3>
+               <CTAButton/>
+              </section>
+              <div className="absolute top-[55%] left-[33%] max-slg:top-[55%]  max-md:hidden ">
+              <Arrrows/>
+              </div>
+              {/* <section className="iphone-cta">
+                <img src="iPhone.svg" alt="" className="w-full" />
+              </section> */}
+        </section>
+         <div className="w-full h-[500px] absolute top-0">
+         <ImageCarousel/>
+         </div>
+
+    </section>
   )
 }
 
