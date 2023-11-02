67419dbaf8b28992835c0853d4f4f207fd574f41
FolashadeOmoloja <omolojashade@gmail.com>
2023-11-02 21:03:13 +0100

    header

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -8,8 +8,8 @@ const Header = () => {
 
   return (
     <div  className='relative h-[830px] max-md:h-[900px] max-xxsm:h-[950px]'>
-    <div className={`transition-div  absolute top-0 w-full `}><FirstSlide/></div>
-    <div className={`transition-div  absolute top-0 w-full hidden`}><SecondSlide/></div>
+    <div className={`transition-div  absolute top-0 w-full hidden `}><FirstSlide/></div>
+    <div className={`transition-div  absolute top-0 w-full `}><SecondSlide/></div>
     </div>
   );
 };
--- components/Header/SecondSlide.tsx
+++ components/Header/SecondSlide.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const SecondSlide = () => {
   return (
-    <div className='bg-blue-500 h-full w-full flex items-center justify-center text-[100px]'>
+    <div className='h-[830px]  max-md:h-[900px] max-xxsm:h-[950px] .bg-headergradient  relative w-full'>
         SecondSlide
     </div>
   )
