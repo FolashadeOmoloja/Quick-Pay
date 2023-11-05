1b99a9e79f8d3b480f82c5cd212313caaea8c870
FolashadeOmoloja <omolojashade@gmail.com>
2023-11-05 20:55:35 +0100

    header

--- components/Header/Floaters.tsx
+++ components/Header/Floaters.tsx
@@ -1,8 +1,34 @@
-import React from 'react'
+'use client'
+import React, { useEffect, useState } from 'react'
+import Floater from './Floater';
+import Floater2 from './Floater2';
+
 
 const Floaters = () => {
+    const [firstFloat, setFirstFloat] = useState(true);
+    const [secondFloat, setSecondFloat] = useState(false);
+  
+    const showTitle = () => {
+      setFirstFloat(!firstFloat);
+      setSecondFloat(!secondFloat);
+    };
+  
+    useEffect(() => {
+      const interval = setInterval(showTitle, 8000); 
+  
+      return () => {
+        clearInterval(interval);
+      };
+    }, [firstFloat, secondFloat]);
   return (
-    <div>Floaters</div>
+    <div className='relative'>
+           {
+            firstFloat && <Floater/>
+           }
+           {
+            secondFloat && <Floater2/>
+           }
+    </div>
   )
 }
 
